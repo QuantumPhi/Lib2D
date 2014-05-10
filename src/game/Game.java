83ba648457ec8be25b4e41f72d31dca4a696c8d8
QuantumPhi <phi.quantum@gmail.com>
2014-05-10 09:34:45 -0700

    Random changes to ordering and stuff

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package game;
 
-/**
- *
- * @author tarikonalan
- */
-public class Game {
+public interface Game {
     
+    void init();
+    void update();
+    void render();
 }
--- src/game/entity/Entity.java
+++ src/game/entity/Entity.java
@@ -1,17 +1,37 @@
 package game.entity;
 
 import game.entity.properties.Gravity;
+import game.util.Vector2D;
 
-public class Entity implements Gravity {
+public abstract class Entity implements Gravity {
     
     protected int x, y;
+    protected Vector2D velocity;
+    protected boolean grav;
     
-    public Entity() { this(0, 0); }
+    public Entity() { 
+        this(0, 0); 
+    }
+    public Entity(boolean _grav) { 
+        this();
+        grav = _grav; 
+    }
+    public Entity(int _x, int _y) { 
+        x = _x;
+        y = _y;
+        init();
+        velocity = new Vector2D(0, 0);
+    }
+    public Entity(int _x, int _y, boolean _grav) { 
+        this(_x,_y);
+        init();
+        grav = _grav; 
+    }
     
-    public Entity(int _x, int _y) { x=_x;y=_y; }
+    public abstract void init(); 
+    public abstract void render();
     
-    @Override
-    public void update(int delta) {
-        y -= Math.round(((double)delta / 100) * G * multiplier);
+    protected void updateValues(int delta) {
+        y = grav ? y-(int)Math.round(((double)delta / 100) * G * multiplier) : y;
     }
 }
