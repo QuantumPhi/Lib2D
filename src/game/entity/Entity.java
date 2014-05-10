2291b2ef30c32ea94daa6a29f440b82142c1e081
QuantumPhi <phi.quantum@gmail.com>
2014-05-09 23:22:53 -0700

    Rudimentary framework

--- src/game/entity/Entity.java
+++ src/game/entity/Entity.java
@@ -1,5 +1,17 @@
 package game.entity;
 
-public class Entity {
+import game.entity.properties.Gravity;
+
+public class Entity implements Gravity {
     
+    protected int x, y;
+    
+    public Entity() { this(0, 0); }
+    
+    public Entity(int _x, int _y) { x=_x;y=_y; }
+    
+    @Override
+    public void update(int delta) {
+        y -= Math.round(((double)delta / 100) * G * multiplier);
+    }
 }
--- src/game/entity/properties/Gravity.java
+++ src/game/entity/properties/Gravity.java
@@ -1,21 +1,10 @@
 package game.entity.properties;
 
-import game.util.Vector2D;
-
-public class Gravity {
+public interface Gravity {
     
-    private final double G = 9.80665;
+    final double G = 9.80665;
     
-    private Vector2D vector;
-    private double multiplier;
+    double multiplier = 1.0;
     
-    public Gravity(double multiplier) {
-        vector = new Vector2D(0, 0);
-    }
-    
-    public void update(int delta) { 
-        vector.setY(-((double)delta / 100 * G * multiplier));
-    }
-    
-    public double getMultiplier() { return multiplier; }
+    void update(int delta);
 }
